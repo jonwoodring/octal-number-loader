b27d1e22882052f0d4291d6c3afda6b2f1361d99
Jon Woodring <woodring.jon@gmail.com>
2017-02-26 23:04:05 -0700

    Identifier fix.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 module.exports = function(source) {
   this.cacheable && this.cacheable();
-  while ((typeof source === "string") && (/\b0[0-7]+\b/.test(source))) {
-    source = source.replace(/\b0([0-7]+)\b/, '0o$1');
+  // I this possibly could still match an exponent
+  // in a floating point: 1e-05
+  while ((typeof source === "string") && (/([^a-zA-Z0-9$_.]|^)0[0-7]+([^a-zA-Z0-9$_.]|$)/.test(source))) {
+    source = source.replace(/([^a-zA-Z0-9$_.]|^)(0)([0-7]+)([^a-zA-Z0-9$_.]|$)/, '$1$2o$3$4')
   }
   return source;
 }
