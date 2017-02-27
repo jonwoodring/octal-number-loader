fbb0d56588378729b56bb8954b9e4789ec759315
Jon Woodring <woodring.jon@gmail.com>
2017-02-26 18:46:54 -0700

    Added \b to test.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = function(source) {
   this.cacheable && this.cacheable();
-  while ((typeof source === "string") && (/\b0[0-7]+/.test(source))) {
-    source = source.replace(/\b0([0-7]+)/, '0o$1');
+  while ((typeof source === "string") && (/\b0[0-7]+\b/.test(source))) {
+    source = source.replace(/\b0([0-7]+)\b/, '0o$1');
   }
   return source;
 }
